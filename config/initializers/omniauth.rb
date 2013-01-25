57fb34e8d29a93f377c2e483636115149a0523a6
Bernardo Dornellas Cysneiros Gomes de Amorim <bamorim2@gmail.com>
2013-01-25 14:59:35 -0200

    App Responde Ai Facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '396887157051732', '4ffb6df4cea8f18949e28fdc5ae95afe'
+  provider :facebook, '323705184406626', '046916a0ea109aa71b3fc1d73970ed7f'
 end
